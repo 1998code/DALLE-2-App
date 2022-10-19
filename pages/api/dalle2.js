3372cb1ba360f04ce6e48c4a6953b63dfed664a7
1998code <54872601+1998code@users.noreply.github.com>
2022-10-19 13:54:38 +0800

    Update dalle2.js

--- pages/api/dalle2.js
+++ pages/api/dalle2.js
@@ -3,5 +3,6 @@ import { Dalle } from "dalle-node"
 export default async function handler(req, res) {
   const dalle = new Dalle(req.query.k);
   const generations = await dalle.generate(req.query.q);
+  console.log(generations);
   res.status(200).json({ result: generations })
 }
