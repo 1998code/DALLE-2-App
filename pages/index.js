8f46ab4c6edaeb86b77899cdff53c0a84304157e
kenny-dinh-codeleap <kenny.dinh@codeleap.de>
2023-08-30 10:27:16 +0700

    Fix typo

--- pages/index.js
+++ pages/index.js
@@ -51,12 +51,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create DALLE 2 App</title>
+        <title>Create Images With DALL-E 2 App</title>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Create images with <span className={styles.titleColor}>DALLE 2</span>
+          Create images with <span className={styles.titleColor}>DALL-E 2</span>
         </h1>
         <p className={styles.description}>
           <input
